cfebf5c821dc069350d1a30fca93f3e84b80308a
jayvyer <javieretcliu@gmail.com>
2023-12-22 10:39:40 -0500

    [SCons] Build and Clean

--- src/Alibi.cc
+++ src/Alibi.cc
@@ -1,6 +1,12 @@
 #include <iostream>
 
 int main(){
-    std::cout << "javier || meow";
+    std::cout << "javier || meow" << std::endl;
     return 0;
 }
+
+// scons -Q
+// scons -c
+
+// https://scons.org/doc/2.3.0/HTML/scons-user/x474.html
+
