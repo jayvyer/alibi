b0963f1edcd08bdff834e79c5837efb02d79be3d
jayvyer <javieretcliu@gmail.com>
2025-03-19 21:32:28 -0400

    [BUILD] Refs: [TicketForBuildSystem] Beginning brewing of CMake

--- src/Alibi.cc
+++ src/Alibi.cc
@@ -4,9 +4,3 @@ int main(){
     std::cout << "javier || meow" << std::endl;
     return 0;
 }
-
-// scons -Q
-// scons -c
-
-// https://scons.org/doc/2.3.0/HTML/scons-user/x474.html
-
---
+++ src/BondMath.cc
@@ -0,0 +1,5 @@
+#include "BondMath.h"
+
+double PriceToYield(){
+    return 5.0;
+}
